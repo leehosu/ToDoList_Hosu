83381db05ac6f393cac60336965e8f93bd8566d8
이호수 <hosu4549@gmail.com>
2019-03-29 20:47:24 +0900

    Success Edit Function

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
-var checkbox = document.createElement("input");
-checkbox.type="checkbox";
-
 function newItem() {
-  var item = document.getElementById("input").value;
-  var ul = document.getElementById("list");
-  var li = document.createElement("li");
+  let item = document.getElementById("input").value;
+  let ul = document.getElementById("list");
+  let li = document.createElement("li");
 
   if(item==='' || item===' '||item ==='  '){
       alert("입력값이 없습니다..");
   }
   else{
-     li.appendChild(document.createTextNode("- " + item));
-     ul.appendChild(li);
+      li.appendChild(document.createTextNode("- " + item));
+      ul.appendChild(li);
   }
 
   document.getElementById("input").value = "";
 
-   var span = document.createElement("SPAN");
-   var markX = document.createTextNode("\u00D7");
-   span.className = "close";
-   span.appendChild(markX);
-   checkbox.checked = false;
-   li.appendChild(span);
+  var delSpan = document.createElement("delSpan");
+  var markX = document.createTextNode("\u00D7");
+  delSpan.className = "close";
+  delSpan.appendChild(markX);
+  li.appendChild(delSpan);
 
+  var editSpan = document.createElement("editSpan");
+  var editBtn = document.createTextNode("EDIT");
+  editSpan.className = "edit";
+  editSpan.appendChild(editBtn);
+  li.appendChild(editSpan);
+
+
+   var close = document.getElementsByClassName("close");
    for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
     }
   }
+  var edit = document.getElementsByClassName("edit");
+
+  for (i = 0; i < edit.length; i++) {
+   edit[i].onclick = function() {
+     var li = this.parentElement;
+     li.contentEditable = 'true';
+   }
+ }
 }
 
-var close = document.getElementsByClassName("close");
-var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
@@ -40,16 +50,12 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-    checkbox.checked = true;
-  }
-}, false);
-
 document.body.onkeyup = function(e) { //엔터키 적용
   if (e.keyCode == 13) {
     newItem();
   }
 };
+
+function updateBtn(){
+
+}
