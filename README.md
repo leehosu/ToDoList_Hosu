bed2f96f6f1da220f2bcfc348720961723864e30
이호수 <hosu4549@gmail.com>
2019-04-01 19:56:30 +0900

    Add delete function

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # ToDoList_Hosu
 
 ### 함수표현식으로 다시 설계 시작!
-1. checkbox
-2. delete
+1. checkbox (완료)
+2. delete (완료)
 3. edit
 
 =======
--- main.js
+++ main.js
@@ -6,13 +6,19 @@ var createNewTaskElement = function(taskString) {
     var listItem = document.createElement("li");
     var checkBox = document.createElement("input");
     var label = document.createElement("label");
+    var deleteButton = document.createElement("button");
+
 
     checkBox.type = "checkbox";
+
+    deleteButton.innerText = "Delete";
+      deleteButton.className = "delete";
 
     label.innerText = taskString;
 
     listItem.appendChild(checkBox);
     listItem.appendChild(label);
+    listItem.appendChild(deleteButton);
 
     document.getElementById("inputText").value = "";
 
@@ -20,9 +26,23 @@ var createNewTaskElement = function(taskString) {
 }
 
 
-var addBtn = function() {
+var addBtn = function(e) {
     var listItem = createNewTaskElement(inputText.value);
     list.appendChild(listItem);
+    btnEvents(listItem);
+}
+
+var deleteBtn = function() {
+    var listItem = this.parentNode;
+    var ul = listItem.parentNode;
+    ul.removeChild(listItem);
+
+}
+
+var btnEvents = function(ListItem) {
+    var deleteButton = ListItem.querySelector("button.delete");
+
+    deleteButton.onclick = deleteBtn;
 }
 
 addButton.onclick = addBtn;
