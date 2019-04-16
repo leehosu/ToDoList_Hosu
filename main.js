7a7c2f635fb58ad57652175702a74c198b206b22
이호수 <hosu4549@gmail.com>
2019-04-17 00:06:27 +0900

    closed #55 deleteIndex

--- main.js
+++ main.js
@@ -1,88 +1,57 @@
-var inputText = document.getElementById("inputText");
-var addButton = document.getElementsByTagName("button")[0];
-var list = document.getElementById("listItem");
+    const inputText = document.querySelector("input.inputText")
+    const addButton = document.querySelector(".addBtn")
+    const list = document.querySelector(".list")
+    const indexList = [];
+    const ENTER = 13
 
-var createInput = function() {
-    var listItem = document.createElement("listItem");
-    var li = document.createElement("li");
-    var span = document.createElement("span");
-    var checkBox = document.createElement("input");
-    var editButton = document.createElement("button");
-    var deleteButton = document.createElement("button");
-    var newInput = inputText.value;
+    addButton.addEventListener('click', addBtnEvent)
 
-    checkBox.type = "checkbox";
-    editButton.innerText = "Edit";
-    editButton.className = "edit";
+    function addBtnEvent() {
+        const {value} = inputText
+        if (!value.trim()) return
+        let templete = `
+            <li class="item-list">
+                <label class="app-list">
+                    <input type="checkbox" class="checkbox">
+                    <span class="spanText">${value}</span>
+                    </label>
+                    <div class="list-btn">
+                    <button class="delete">Delete</button>
+                    <button class="edit">Edit</button>
+                </div>
+            </li>
+        `
 
-    deleteButton.innerText = "Delete";
-    deleteButton.className = "delete";
+        inputText.value = ""
 
-    span.appendChild(document.createTextNode(" " + newInput));
-
-    li.appendChild(checkBox);
-    li.appendChild(deleteButton);
-    li.appendChild(editButton);
-    li.appendChild(span);
-    listItem.appendChild(li);
-
-    document.getElementById("inputText").value = "";
-
-    btnEvents(listItem);
-    return listItem;
-}
-
-var addBtn = function() {
-    if (!inputText.value.trim()) return;
-    var listItem = createInput(inputText.value);
-
-    list.appendChild(listItem);
-}
-
-var editBtn = function() {
-    var listItem = this.parentNode;
-    var spanText = listItem.querySelector("span");
-    var editButton = listItem.querySelector("button.edit");
-
-    spanText.contentEditable = 'true';
-
-    console.log(spanText);
-    spanText.style.color = 'gray';
-    editButton.onclick = editTextBtn;
-}
-
-var editTextBtn = function() {
-    var listItem = this.parentNode;
-    var spanText = listItem.querySelector("span");
-    var editButton = listItem.querySelector("button.edit");
-
-    spanText.contentEditable = 'false';
-    spanText.style.color = 'black';
-  
-    editButton.onclick = editBtn;
-
-}
-
-var deleteBtn = function() {
-    var listItem = this.parentNode;
-    var li = listItem.parentNode;
-
-    li.removeChild(listItem);
-}
-
-var btnEvents = function(listItem) {
-    var deleteButton = listItem.querySelector("button.delete");
-    deleteButton.onclick = deleteBtn;
-
-    var editButton = listItem.querySelector("button.edit");
-    editButton.onclick = editBtn;
-}
-
-inputText.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("AddBtn").click();
+        list.innerHTML += templete
+        indexList.push(value)
+        var deleteButton = list.querySelectorAll(".delete")
+        deleteButton.forEach((element, index) => {
+            deleteButton[index].addEventListener('click', deleteBtnEvent)
+        })
+        console.log(indexList)
+        return indexList
     }
-});
 
-addButton.onclick = addBtn;
+    function deleteBtnEvent() {
+        const itemList = document.querySelectorAll(".item-list")
+        const parent = this.parentNode.parentNode
+
+        itemList.forEach((element, index) => {
+            if (element === parent) {
+                index
+                parent.remove()
+                indexList.splice(parent,1)
+            }
+        })
+    }
+
+
+
+    inputText.addEventListener("keyup", function(event) {
+        if (event.keyCode === ENTER) {
+            event.preventDefault()
+            addBtnEvent()
+        }
+    })
